e14525830ea86d937872a90440c504edc59e9c43
Hattyoriiiiiii <www.tatsuya92@gmail.com>
2022-04-08 13:54:25 +0900

    modified

--- mkproj.sh
+++ mkproj.sh
@@ -15,7 +15,7 @@ function mkproj()
 
 ########## <<< config >>> ##########
 
-DIR=(data log others summary scripts QualityAssessment 01_qc/{before/multiqc_before,after/multiqc_after} 02_trim 03_align 04_RSEM 05_rsem_counts 06_diff 07_topgo 08_reactomepa notebooks/{data,figures})
+DIR=(data log others summary QualityAssessment 01_qc/{before/multiqc_before,after/multiqc_after} 02_trim 03_align 04_RSEM 05_rsem_counts 06_diff 07_topgo 08_reactomepa notebooks/{data,figures})
 LINK=(01_qc/before/multiqc_before 01_qc/after/multiqc_after others scripts summary log notebooks QualityAssessment)
 
 ########## Making directories ##########
@@ -31,6 +31,9 @@ touch README.md others/{sample.txt,rename.sh,rename.csv,SRR_Acc_List.txt}
 # git clone
 
 ########## change directory to make symbolic link ##########
+printf "\n <<< Type project name (e.g. RNA_Stra8) >>> \n\n"
+read projname
+
 mkproj REPORTS_${projname}; cd REPORTS_${projname}
 
 for path in "${LINK[@]}"; do
